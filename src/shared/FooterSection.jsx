584babfe40c6cae1c10e1db30003c2d48ea6bf1e
abukawsar45 <kawsar1146236@gmail.com>
2023-05-18 16:16:39 +0600

    3rd commit navbar and footer setup

--- src/shared/FooterSection.jsx
+++ src/shared/FooterSection.jsx
@@ -1,26 +1,34 @@
 import { Footer } from "flowbite-react";
-
+import { FaFacebook, FaTwitter, FaGithub, FaInstagram } from 'react-icons/fa';
 
 const FooterSection = () => {
   return (
-    <div>
+    <div className=" ">
       <Footer container={true}>
         <div className='w-full'>
-          <div className='grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1'>
-            <div>
+          <div className='w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1'>
+            <div className="flex  flex-col md:flex-row my-4 md:my-0 justify-start md:items-center">
               <Footer.Brand
-                href='https://flowbite.com'
-                src='https://flowbite.com/docs/images/logo.svg'
+                  
+            src='https://i.ibb.co/HXRYSmH/logo22.jpg'
+            className='w-2/4 md:w-3/4 h-auto'
                 alt='Flowbite Logo'
-                name='Flowbite'
               />
+              <h3 className="text-2xl font-extrabold md:text-3xl font-bold">Best Toys Shop</h3>
             </div>
-            <div className='grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6'>
+            <div className='grid grid-cols-1 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6'>
               <div>
                 <Footer.Title title='about' />
                 <Footer.LinkGroup col={true}>
-                  <Footer.Link href='#'>Flowbite</Footer.Link>
-                  <Footer.Link href='#'>Tailwind CSS</Footer.Link>
+                  <Footer.Link href='#'>
+                    <span className='font-bold'>Head Office:</span>Dhaka,Bonani
+                  </Footer.Link>
+                  <Footer.Link href='#'>
+                    <span className='font-bold'>Branch:</span> Uttara,1
+                    <br />
+                    <br />
+                    Dhaka,Bangladesh
+                  </Footer.Link>
                 </Footer.LinkGroup>
               </div>
               <div>
@@ -28,26 +36,27 @@ const FooterSection = () => {
                 <Footer.LinkGroup col={true}>
                   <Footer.Link href='#'>Github</Footer.Link>
                   <Footer.Link href='#'>Discord</Footer.Link>
+                  <Footer.Link href='#'>FaceBook</Footer.Link>
                 </Footer.LinkGroup>
               </div>
               <div>
                 <Footer.Title title='Legal' />
                 <Footer.LinkGroup col={true}>
                   <Footer.Link href='#'>Privacy Policy</Footer.Link>
-                  <Footer.Link href='#'>Terms & Conditions</Footer.Link>
+                  <Footer.Link href='#'>Terms & </Footer.Link>
+                  <Footer.Link href='#'>Conditions</Footer.Link>
                 </Footer.LinkGroup>
               </div>
             </div>
           </div>
           <Footer.Divider />
           <div className='w-full sm:flex sm:items-center sm:justify-between'>
-            <Footer.Copyright href='#' by='Flowbite™' year={2022} />
+            <Footer.Copyright href='#' by='Best Toys House' year={2023} />
             <div className='mt-4 flex space-x-6 sm:mt-0 sm:justify-center'>
-              {/* <Footer.Icon href='#' icon={BsFacebook} />
-              <Footer.Icon href='#' icon={BsInstagram} />
-              <Footer.Icon href='#' icon={BsTwitter} />
-              <Footer.Icon href='#' icon={BsGithub} />
-              <Footer.Icon href='#' icon={BsDribbble} /> */}
+              <FaFacebook className='cursor-pointer h-6 q-6 text-teal-400' />{' '}
+              <FaTwitter className='cursor-pointer h-6 q-6 text-cyan-500' />{' '}
+              <FaGithub className='cursor-pointer h-6 q-6 text-slate-950' />{' '}
+              <FaInstagram className='cursor-pointer h-6 q-6 text-red-500' />
             </div>
           </div>
         </div>
